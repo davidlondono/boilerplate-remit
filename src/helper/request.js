8c428e779d1d642c52d6bc92e27c3911ba49500f
Jinme Mirabal <mirabalj@gmail.com>
2017-10-17 10:10:00 -0500

    Add helper to envolve request of remit

---
+++ src/helper/request.js
@@ -0,0 +1,25 @@
+const logger = require('../logger');
+const BusinessError = require('../BusinessError');
+const appConfig = require('../config/app');
+
+const { app: appName, service: serviceOwnName } = appConfig;
+
+const parseString = s => s.replace(/\.?([A-Z]+)/g, (x, y) => `.${y.toLowerCase()}`);
+
+const request = ({ serviceName, interfaceMethod, client, options = {} }) => {
+  const endpoint = parseString(interfaceMethod);
+  const queueName = `${appName}.${serviceName || serviceOwnName}.${endpoint}`;
+  const run = client.request(queueName).options(options);
+
+  return async (msg) => {
+    logger.trace('Publish on interface', queueName, 'with data', msg);
+    const response = await run(msg);
+    const { data, error, ok } = response;
+    if (!ok) {
+      throw new BusinessError(error);
+    }
+    return data;
+  };
+};
+
+module.exports = request;
